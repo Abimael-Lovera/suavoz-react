9eb2dd26054210c2f63b317394a9213447457cb6
Abimael-Lovera <abimaellovera@gmail.com>
2022-03-21 22:25:34 -0300

    Logo no Footer e links consertados

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,56 +1,44 @@
-import React from "react";
-import "../../components/Layout.css";
+import '../../components/Layout.css';
+import suavozbege from '../../assets/suaVoz-bege.png';
 
 export default function Footer() {
-  return (
-    <>
-      <footer className=" footer text-muted">
-        <div className="container">
-          &copy; 2022 - SuaVoz -{" "}
-          <a asp-area="" asp-controller="Home" asp-action="Privacy">
-            Privacidade
-          </a>{" "}
-          /
-          <a asp-area="" asp-controller="Home" asp-action="Termos">
-            Termos de Uso
-          </a>
-        </div>
-      </footer>
-      <footer class="footer__container">
-        <div class="footer__content container">
-          <div class="footer__header">
-            <img src="~/img/suaVoz-bege.png" alt="" class="footer__logo" />
-            <p class="footer__paragraph">&copy; 2021 Company, Inc</p>
-          </div>
-          <ul class="footer__body">
-            <li class="footer__item">
-              <a href="#" class="footer__links">
-                <i class="fa-brands fa-instagram social__media"></i>
-              </a>
-            </li>
-            <li class="footer__item">
-              <a href="#" class="footer__links">
-                <i class="fa-brands fa-facebook social__media"></i>
-              </a>
-            </li>
-            <li class="footer__item">
-              <a href="#" class="footer__links">
-                <i class="fa-brands fa-twitter social__media"></i>
-              </a>
-            </li>
-            <li class="footer__item">
-              <a href="#" class="footer__links">
-                <i class="fa-brands fa-youtube social__media"></i>
-              </a>
-            </li>
-            <li class="footer__item">
-              <a href="#" class="footer__links">
-                <i class="fa-brands fa-twitch social__media"></i>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </footer>
-    </>
-  );
+	return (
+		<>
+			<footer class='footer__container'>
+				<div class='footer__content container'>
+					<div class='footer__header'>
+						<img src={suavozbege} alt='' class='footer__logo' />
+						<p class='footer__paragraph'>&copy; 2022 Company, Inc</p>
+					</div>
+					<ul class='footer__body'>
+						<li class='footer__item'>
+							<a href='#.' class='footer__links'>
+								<i class='fa-brands fa-instagram social__media'></i>
+							</a>
+						</li>
+						<li class='footer__item'>
+							<a href='#.' class='footer__links'>
+								<i class='fa-brands fa-facebook social__media'></i>
+							</a>
+						</li>
+						<li class='footer__item'>
+							<a href='#.' class='footer__links'>
+								<i class='fa-brands fa-twitter social__media'></i>
+							</a>
+						</li>
+						<li class='footer__item'>
+							<a href='#.' class='footer__links'>
+								<i class='fa-brands fa-youtube social__media'></i>
+							</a>
+						</li>
+						<li class='footer__item'>
+							<a href='#.' class='footer__links'>
+								<i class='fa-brands fa-twitch social__media'></i>
+							</a>
+						</li>
+					</ul>
+				</div>
+			</footer>
+		</>
+	);
 }
