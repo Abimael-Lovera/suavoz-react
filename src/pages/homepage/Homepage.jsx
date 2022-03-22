db5c2766d4ad468f82e95efb543648ec67304704
Abimael-Lovera <abimaellovera@gmail.com>
2022-03-22 15:47:39 -0300

    componentes formContato e Parceiros criados

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,10 +1,12 @@
-import Hero from './hero/Hero';
-import React from 'react';
-import Conquistas from './conquistas/Conquistas';
-import StartTheFeaturettes from './start the featurettes/StartTheFeaturettes';
-import Eventos from './eventos/Eventos';
-import Header from '../../components/header/Header';
-import Footer from '../../components/footer/Footer';
+import Hero from "./hero/Hero";
+import React from "react";
+import Conquistas from "./conquistas/Conquistas";
+import StartTheFeaturettes from "./start the featurettes/StartTheFeaturettes";
+import Eventos from "./eventos/Eventos";
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+import FormContato from "./FormContato/FormContato";
+import Parceiros from "./Parceiros/Parceiros";
 
 export default function Homepage() {
 	return (
@@ -13,6 +15,8 @@ export default function Homepage() {
 			<Conquistas></Conquistas>
 			<StartTheFeaturettes></StartTheFeaturettes>
 			<Eventos></Eventos>
+			<Parceiros />
+			<FormContato />
 			<Footer></Footer>
 		</>
 	);
