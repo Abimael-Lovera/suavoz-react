197473ef92350d85c3d02af41bc653a9338703a8
Abimael-Lovera <abimaellovera@gmail.com>
2022-03-22 12:38:49 -0300

    Limpando codigo preparando para refatorar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-import './App.css';
-import React from 'react';
-import SeusDireitos from './SeuDireitos/SeusDireitos';
-import SobreNos from './SobreNos/SobreNos';
-import Homepage from './components/homepage/Homepage';
-
 function App() {
 	return (
 		<div>
-			<Homepage />
-			{/* <SobreNos /> */}
-			<SeusDireitos />
+			<h1>Limpando Projeto</h1>
 		</div>
 	);
 }
