079226b5285ba0ae0ac5571600fee4e7793c0208
Abimael-Lovera <abimaellovera@gmail.com>
2022-03-21 23:08:58 -0300

    Criando  Rotas home SeusDireitos SobreNos

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
-import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import SeusDireitos from './SeuDireitos/SeusDireitos';
 import SobreNos from './SobreNos/SobreNos';
 import Homepage from './components/homepage/Homepage';
 
 function App() {
 	return (
-		<div>
-			<Homepage />
-			{/* <SobreNos /> */}
-			{/* <SeusDireitos /> */}
-		</div>
+		<>
+			<Routes>
+				<Route path='/' element={<Homepage />} />
+				<Route path='/seusdireitos' element={<SeusDireitos />} />
+				<Route path='/sobrenos' element={<SobreNos />} />
+			</Routes>
+		</>
 	);
 }
 
